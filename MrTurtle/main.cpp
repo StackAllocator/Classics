ac31dabc8e67837ec15dcbc284798c5a71a12dbe
Johannes <Johannes.Elsing@gmx.de>
2024-03-04 21:17:46 +0100

    initial commit

---
+++ MrTurtle/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <raylib.h>
+#include "game.hpp"
+
+auto main() -> int {
+
+  constexpr int screen_width = 1280;
+  constexpr int screen_height = 800;
+
+  // initialize window
+  InitWindow(screen_width, screen_height, "Mr Turtle");
+  Vector2 player_position = {50, 200};
+  SetTargetFPS(60);
+
+  Game *game = new (std::nothrow) Game();
+
+  while (!WindowShouldClose()) {
+    game->run();
+  }
+
+  // cleanup
+  game->unload_textures();
+  delete game;
+  CloseWindow();
+
+  return 0;
+}
